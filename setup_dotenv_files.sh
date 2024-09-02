0fbd88a5e55c6f62c2ea7d6032af95c428e35531
Juan Shotyou <juan@shot.you>
2024-09-02 16:51:50 +0100

    Edited script + added required fonts into repo

--- setup_dotenv_files.sh
+++ setup_dotenv_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rsync -ax --exclude .git --exclude setup_dotenv_files.sh ./ ~/
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+rsync -ax --exclude .git --exclude setup_dotenv_files.sh --exclude vim_fonts ./ ~/
+curl --no-progress-meter -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo 'For VIM: Remember to run ":PlugInstall" on first run to set up the themes and plugins!'
