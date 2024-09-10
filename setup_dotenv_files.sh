1f8bac1678e78ebdcba6d18d6548c5c5fe98c2bd
Vlad R <vlad.raducanu@outlook.com>
2024-09-10 10:45:53 +0100

    Testing repo mirroring

--- setup_dotenv_files.sh
+++ setup_dotenv_files.sh
@@ -4,4 +4,6 @@ rsync -ax --exclude .git --exclude setup_dotenv_files.sh --exclude vim_fonts ./ 
 curl --no-progress-meter -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo 'For VIM: Remember to run ":PlugInstall" on first run to set up the themes and plugins!'
+echo 'VIM: Remember to run ":PlugInstall" on first run to set up the themes and plugins!'
+echo 'Ignore the error about "gruvbox" - it will be fixed after you install the plugins.'
+
